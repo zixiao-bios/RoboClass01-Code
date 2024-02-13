234e0d62939162c094fc926b1b9b546c3560d49f
zixiao_bios <zixiao_bios@outlook.com>
2024-02-14 00:09:29 +0800

    add 6-1-class_ws

---
+++ 6-1-class_ws/cnn.py
@@ -0,0 +1,73 @@
+import torch.nn as nn
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1, 
+                out_channels=16, 
+                kernel_size=5, 
+                stride=1, 
+                padding=2, 
+            ), 
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size=2), 
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2), 
+            nn.ReLU(), 
+            nn.MaxPool2d(2), 
+        )
+
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)
+
+        return self.out(x)
+
+
+class LeNet5(nn.Module):
+    def __init__(self, num_classes):
+        super(LeNet5, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(6),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        
+        self.fc = nn.Linear(256, 120)
+        self.relu = nn.ReLU()
+
+        self.fc1 = nn.Linear(120, 84)
+        self.relu1 = nn.ReLU()
+
+        self.fc2 = nn.Linear(84, num_classes)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        out = self.relu(out)
+        out = self.fc1(out)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        return out
---
+++ 6-1-class_ws/predict.py
@@ -0,0 +1,82 @@
+import torch
+from pathlib import Path
+from torchvision.io import read_image
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+
+from cnn import SimpleCNN, LeNet5
+
+# 权重文件的文件名
+# weights_file = '1707838716_epoch_9.pt'
+weights_file = '1707839915_epoch_5.pt'
+
+# 获取权重文件的路径
+current_dir_path = Path(__file__).resolve().parent
+model_weights_path = current_dir_path / 'weights' / weights_file
+
+# 输入图片文件夹的路径
+input_imgs_dir = current_dir_path / '..' / 'data' / 'my_write_num'
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_input_data(input_imgs_dir, img_size):
+    # list，存储所有输入图片的路径
+    imgs_path = input_imgs_dir.glob('*.jpg')
+    
+    # 转换输入图片的流水线
+    transform = transforms.Compose([
+        # 变为灰度图
+        transforms.Grayscale(), 
+        # 修改尺寸
+        transforms.Resize(img_size, antialias=True), 
+    ])
+
+    # 读取输入图片，并进行预处理
+    input_imgs = []
+    for img_path in imgs_path:
+        # 读取图片文件
+        img = read_image(str(img_path))
+
+        # 应用transform流水线到图片
+        img = transform(img)
+
+        # 从 0-255 归一化为 0-1
+        img = img.type(torch.float32) / 255
+
+        # 灰度反转
+        img = img * -1 + 1
+        
+        input_imgs.append(img)
+
+    # (n, 1, 28, 28)
+    data = torch.stack(input_imgs).to(device)
+    return data
+
+
+def main():
+    data = get_input_data(input_imgs_dir, (28, 28))
+
+    writer = SummaryWriter(comment=f'_predict_{weights_file}_LeNet5')
+
+    # model = SimpleCNN().to(device)
+    model = LeNet5(10).to(device)
+    model.load_state_dict(torch.load(model_weights_path))
+    model.eval()
+
+    with torch.no_grad():
+        # (b, 10)
+        output = model(data)
+
+        # (b, 1)
+        pred = output.argmax(dim=1, keepdim=True)
+
+    for i in range(10):
+        mask = (pred.view(-1) == i)
+        print(mask.sum())
+        if mask.sum() > 0:
+            writer.add_images(f'num={i}', data[mask])
+
+
+if __name__ == '__main__':
+    main()
